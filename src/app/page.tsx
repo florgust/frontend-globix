3903e79fa3eda0a37ad4460714a795b64834690e
Flor-Gust <luan.flor05@gmail.com>
2025-03-16 17:58:00 -0300

    SCRUM-53: Criando casca do projeto

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// src/app/page.tsx
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Bem-vindo ao meu projeto Next.js com Tailwind!</h1>
+    </div>
+  );
+}
