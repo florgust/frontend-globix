50ab3030c952c3736ec9827240d22149bb48beaf
Arthur Ramos <rmsarthur1@gmail.com>
2025-04-04 10:49:57 -0300

    SCRUM-63: Mudança de fonte para Quicksand, aplicação de gradiente nos botões de navegação entre modais e estilização da tabela

--- src/app/detalhes/page.tsx
+++ src/app/detalhes/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./styles.module.css";
 
-export default function Location() {
+export default function Detalhes() {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
