6a401960ab7386185292849288496a10bb46e8d2
barbrete <contagame011@gmail.com>
2025-04-08 19:04:21 -0300

    style(home_page): adjust indentation and spacing

--- src/app/home_page/page.tsx
+++ src/app/home_page/page.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Briefcase } from "lucide-react";
 import { UserRoundPlus } from "lucide-react";
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 
-  const carouselImages = [
-    "/images/carousel1.jpg",
-    "/images/carousel2.jpg",
-    "/images/carousel3.jpg",
+  const carrosselImages = [
+    "/images/rifaina-capa.svg",
+    "/images/carrossel.png",
+    "/images/marco.jpg",
   ];
 
 export default function HomePage() {
@@ -20,30 +14,41 @@ export default function HomePage() {
         
       <div className="flex min-h-screen items-center justify-center bg-blue-500">
 
-        <button className="block items-center gap-3 px-16 py-10 bg-gray-200 text-gray-800 rounded-lg shadow-lg hover:bg-gray-300 transition">           
-            <Briefcase className="h-12 w-12 text-blue-800" />
-            Criar Viagem
-        </button>
-            
-        <button className="block items-center gap-3 px-16 py-10 bg-green-200 text-gray-800 rounded-lg shadow-lg hover:bg-green-300 transition">           
-            <UserRoundPlus className="h-12 w-12 text-blue-800" />
-            Participar de Viagem
-        </button>
+        <div className="absolute top-25 left-10 flex space-x-6">
+          <button className="block mb-180 items-center gap-3 px-16 py-10 bg-gray-200 text-gray-800 rounded-lg shadow-lg hover:bg-gray-300 transition">           
+              <Briefcase className="h-12 w-12 text-blue-800" />
+              Criar Viagem
+          </button>
+              
+          <button className="block mb-180 items-center gap-3 px-16 py-10 bg-green-200 text-gray-800 rounded-lg shadow-lg hover:bg-green-300 transition">           
+              <UserRoundPlus className="h-12 w-12 text-blue-800" />
+              Participar de Viagem
+          </button>
+        </div>
 
-        <div className="text-center">
-            <h1 className="text-6xl font-extrabold text-white drop-shadow-lg animate-bounce">
+  
+        <div className="text-center mt-20">
+            <h1 className="text-6xl mb-10 font-extrabold text-white drop-shadow-lg">
                 Viagens Disponíveis para Você
             </h1>
 
-            <Carousel>
+            <Carousel className="flex flex-col items-center">
                 <CarouselContent>
-                    <CarouselItem>...</CarouselItem>
-                    <CarouselItem>...</CarouselItem>
-                    <CarouselItem>...</CarouselItem>
+                  {carrosselImages.map((image, index) => (
+                    <CarouselItem key={index}>
+                      <div className="bg-gradient-to-b from-blue-700 via-blue-600 to-blue-400 w-[75rem] h-[34rem] mx-auto rounded-lg flex items-center justify-center">
+                        <img
+                          src={image}
+                          alt={`Imagem ${index + 1}`}
+                          className="w-[61rem] h-[29.875rem] mx-auto rounded-lg "
+                          />
+                      </div>
+                    </CarouselItem>
+                  ))}
                 </CarouselContent>
 
-                <CarouselPrevious />
-                <CarouselNext />
+                <CarouselPrevious className="absolute left-260 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg hover:bg-gray-200 transition" />
+                <CarouselNext className="absolute right-260 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-lg hover:bg-gray-200 transition" />
             </Carousel>
         </div>
       </div>
