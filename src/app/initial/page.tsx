3b421513b5adfb9569ec6b0d8a702fe60dfab9ea
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-04-15 18:05:05 -0300

    SCRUM-79: Criação do Rodapé

--- src/app/initial/page.tsx
+++ src/app/initial/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function InitialPage() {
   return (
     <main>
       <Header />
       <Hero />
+      <Footer />
     </main>
   );
 }
