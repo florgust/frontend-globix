c7934162dd2b3d8059db4ce1143c37ebb1007a04
Arthur Ramos <rmsarthur1@gmail.com>
2025-04-29 15:15:51 -0300

    SCRUM-88: Implementando e atualizando o modal

--- src/app/travel_transport/page.tsx
+++ src/app/travel_transport/page.tsx
@@ -16,7 +16,7 @@ export default function TravelTransport() {
         <Header/>
         
         <h1
-            className="absolute w-[34.5rem] h-[3.125rem] top-[8.625rem] left-[17.1875rem] text-white text-[2.5rem] font-quicksand font-bold leading-[1] tracking-[0px] text-center"
+            className="absolute w-[34.5rem] h-[3.125rem] top-[8.625rem] left-[17.1875rem] text-white font-quicksand font-bold text-[2.5rem] leading-[1] tracking-[0px] text-center"
         >
             Criar Viagem - TRANSPORTE
         </h1>
@@ -32,83 +32,18 @@ export default function TravelTransport() {
               className="absolute w-[40.875rem] h-[6.25rem] top-[2rem] left-[7rem] text-center"
             >
             <h2 
-              className="text-white font-quicksand font-bold text-[2.5rem] leading-[1] tracking-[0px] text-center"
+              className="text-white font-quicksand font-bold text-[2.5rem] leading-[1] tracking-[0] text-center"
             >
-              Qual será o transporte utilizado na viagem?
+              Qual será o transporte utilizado <br></br> na viagem?
             </h2>
 
 
             </div>
             
-            
-            <div
-                className="absolute top-[12rem] left-[7rem] flex space-x-4"
-            >
-                <div className="flex flex-col items-center">
-                  <div className="w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
-                    <img 
-                      src="/images-travel_transport/aviao.svg" 
-                      alt="Avião" 
-                      className="w-[4.375rem] h-[4.375rem] transform rotate"
-                    />
-                  </div>
-                  <span className="text-white mt-2">Avião</span>
-                </div>
-                <div className="flex flex-col items-center">
-                  <div className="w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
-                    <img 
-                      src="/images-travel_transport/onibus.svg" 
-                      alt="Ônibus" 
-                      className="w-[4.375rem] h-[4.375rem]"
-                    />
-                  </div>
-                  <span className="text-white mt-2">Ônibus</span>
-                </div>
-                <div className="flex flex-col items-center">
-                  <div className="w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
-                    <img 
-                      src="/images-travel_transport/carro.svg" 
-                      alt="Carro" 
-                      className="w-[4.375rem] h-[4.375rem]"
-                    />
-                  </div>
-                  <span className="text-white mt-2">Carro</span>
-                </div>
-                <div className="flex flex-col items-center">
-                  <div className="w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
-                    <img 
-                      src="/images-travel_transport/trem.svg" 
-                      alt="Trem" 
-                      className="w-[4.375rem] h-[4.375rem]"
-                    />
-                  </div>
-                  <span className="text-white mt-2">Trem</span>
-                </div>
-                <div className="flex flex-col items-center">
-                  <div className="w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
-                    <img 
-                      src="/images-travel_transport/navio.svg" 
-                      alt="Navio" 
-                      className="w-[4.375rem] h-[4.375rem]"
-                    />
-                  </div>
-                  <span className="text-white mt-2">Navio</span>
-                </div>
-                <div className="flex flex-col items-center">
-                  <div className="w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
-                    <img 
-                      src="/images-travel_transport/outro.svg" 
-                      alt="Outro" 
-                      className="w-[4.375rem] h-[4.375rem]"
-                    />
-                  </div>
-                  <span className="text-white mt-2">Outro</span>
-                </div>
-            </div>
         </div>
 
         <div
-            className="absolute w-[52.5rem] h-[4.741rem] top-[47rem] left-[23.75rem] border-[0.03125rem] border-[#C4C4C4] rounded-[0.5rem] p-[0.625rem] gap-[0.625rem] bg-[#00FF4D] z-10"
+            className="cursor-pointer absolute w-[52.5rem] h-[4.741rem] top-[47rem] left-[23.75rem] border-[0.03125rem] border-[#C4C4C4] rounded-[0.5rem] p-[0.625rem] gap-[0.625rem] bg-[#00FF4D] z-10"
         >
             <h2 className="text-center text-[#0F2976] font-quicksand font-bold text-[2.5rem] leading-[1.2] tracking-[0]">
               Próximo
@@ -121,23 +56,46 @@ export default function TravelTransport() {
 
         <div className="absolute top-40 left-105 flex space-x-6">
 
-          
-
-          <Modal isOpen={openModal}>
-            <h1 className="text-[#0F2976] text-4xl mb-3 text-center mb-10">Insira o código da Viagem que você <br/> quer participar</h1>
+            <Modal isOpen={openModal}>
+            <h1 className="text-[#0F2976] text-4xl mb-3 text-center mb-10">Insira a descrição do Transporte:</h1>
             <input
               type="text"
-              placeholder="Insira o código da viagem"
-              className=" w-[30rem] h-[6rem] p-2 rounded-full bg-[#0F2976] focus:outline-none text-white placeholder-gray-200 text-2xl text-center"
+              placeholder="Insira mais detalhes sobre o transporte, como: Empresa do transporte, Identificação do transporte e Número da passagem."
+              className="w-[38rem] h-[12rem] p-2 bg-[#0F2976] text-white placeholder-gray-200 rounded-[4.375rem] resize-none"
             />
-            <button onClick={ () => {setOpenModal(false)}}>
-              <X className="absolute top-10 right-10 w-12 h-12 text-[#6C727F]"/>
+            <button onClick={() => { setOpenModal(false); }}>
+              <X className="absolute top-10 right-10 w-12 h-12 text-[#6C727F]" />
             </button>
-          </Modal>
+            </Modal>
+
+            <div
+            className="absolute top-[19rem] flex space-x-6"
+            >
+            {["avião", "ônibus", "carro", "trem", "navio", "outro"].map((item, index) => (
+              <div
+              key={index}
+              className="flex flex-col items-center"
+              onClick={() => {
+              setOpenModal(true);
+                const selectedOption = item; // Guarda o nome da opção na variável
+              }}
+              >
+              <div className="hover:scale-110 transition-transform cursor-pointer w-[6.125rem] h-[6.125rem] bg-white rounded-full flex items-center justify-center">
+              <img
+              src={`/images-travel_transport/${item}.svg`}
+              alt={item.charAt(0).toUpperCase() + item.slice(1)}
+              className="w-[4.375rem] h-[4.375rem]"
+              />
+              </div>
+              <span className="text-white mt-2">
+              {item.charAt(0).toUpperCase() + item.slice(1)}
+              </span>
+              </div>
+            ))}
+            </div>
 
         </div>
-  
-        
+
       </div>
     );
   }
