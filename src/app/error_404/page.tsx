512b001eece10ce798d3260393f5c4a529b7e602
florgust <luan.flor05@gmail.com>
2025-05-13 20:21:34 -0300

    SCRUM-104: Adicionando footer

--- src/app/error_404/page.tsx
+++ src/app/error_404/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import React from "react";
 import SidebarMenu from "../../components/ui/SidebarMenu";
-import {HeaderPages} from "@/components/ui/header";
+import { HeaderPages } from "@/components/ui/header";
+import Footer from "@/components/ui/Footer";
 
 export default function TripTransport() {
     return (
@@ -11,12 +12,12 @@ export default function TripTransport() {
             <div className="flex flex-col items-center w-full bg-gradient-to-b from-[#1C4CDC] to-[#0F2976]">
                 <HeaderPages />
 
-                <div className="mt-25 text-[#A7FF84] text-center">
+                <div className="mt-45 text-[#A7FF84] text-center">
                     <p className="font-bold text-[1.3rem]">A página que você procura não pode ser encontrada</p>
                     <p className="pt-3">ERRO 404</p>
                 </div>
 
-                <div className="absolute flex justify-center items-center mt-15 pointer-events-none">
+                <div className="absolute flex justify-center items-center mt-35 pointer-events-none">
                     <img
                         src="/images-error_404/error404.svg"
                         alt="Erro 404"
@@ -24,15 +25,16 @@ export default function TripTransport() {
                     />
                 </div>
 
-                <div className="flex justify-center space-x-4 gap-5 text-[#FFFFFF] mt-87">
+                <div className="flex justify-center space-x-4 gap-5 text-[#FFFFFF] mt-87 mb-40">
                     <button className="w-70 h-14 border-2 border-[#FFFFFF] rounded-[6.25rem] cursor-pointer hover:scale-110 flex items-center justify-center gap-2">                            <img
-                            src="/images-error_404/seta.svg"
-                            alt="Seta"
-                            className="w-5 h-5"
-                        />
-                        <span className="text-lg">Voltar para Página Inicial</span>
+                        src="/images-error_404/seta.svg"
+                        alt="Seta"
+                        className="w-5 h-5"
+                    />
+                        <a href="/home_page" className="text-lg">Voltar para Página Inicial</a>
                     </button>
                 </div>
+                <Footer />
             </div>
         </div>
     );
