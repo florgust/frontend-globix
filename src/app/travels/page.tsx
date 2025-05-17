37404fb9fb7dbfa278d1e77e790cd97e1953ff54
barbrete <barbara.cobo@estudante.iftm.edu.br>
2025-05-17 02:35:41 -0300

    SCRUM-95: Terminado modal de Promover Organizadores e Adicionado botão de finalizar viagem

--- src/app/travels/page.tsx
+++ src/app/travels/page.tsx
@@ -3,13 +3,25 @@
 import SidebarMenu from '@/components/ui/SidebarMenu';
 import { UserList, RequestList } from '@/components/ui/UserList';
 import { IconButton } from '@/components/ui/button';
-import { Modal, PromoteOrganizerModal } from '@/components/ui/modal';
-import { List, Plus, X } from 'lucide-react';
+import { PromoteOrganizerModal } from '@/components/ui/modal';
+import { List, Plus } from 'lucide-react';
 import { useState } from 'react';
 
 export default function DetailsPage() {
 
     const [isModalOpen, setIsModalOpen] = useState(false);
+
+    async function promoverUsuario(id_usuario: number) {
+        await fetch(`/api/usuarios/${id_usuario}/promover`, {
+            method: "POST",
+        });
+    }
+
+    async function removerUsuario(id_usuario: number) {
+        await fetch(`/api/usuarios/${id_usuario}/remover`, {
+            method: "POST",
+        });
+    }
 
     const detalhesViagem = {
         viagem: {
@@ -48,7 +60,7 @@ export default function DetailsPage() {
             },
             {
                 id_usuario: 2,
-                nome: "Lucas Silva",        
+                nome: "Lucas Silva",
                 email: "lucas@email.com.br",
                 tipo: "OrganizadorPromovido",
                 foto: "/images-travel/images-user/user_patrick.png",
@@ -105,17 +117,24 @@ export default function DetailsPage() {
     const [convidados, setConvidados] = useState(detalhesViagem.usuario);
     const [solicitacoes, setSolicitacoes] = useState(detalhesViagem.solicitacoes);
 
-    const handleAccept = (id: number) => {
+    const handleAccept = async (id: number) => {
+        await fetch(`/api/viagens/aceitar-solicitacao`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ id_usuario: id, id_viagem: detalhesViagem.viagem.id_viagem }),
+        });
+
         const usuarioAceito = solicitacoes.find((solicitacao) => solicitacao.id_usuario === id);
         if (usuarioAceito) {
-            setConvidados([...convidados, usuarioAceito]); // Adiciona aos convidados
-            setSolicitacoes(solicitacoes.filter((solicitacao) => solicitacao.id_usuario !== id)); // Remove das solicitações
+            setConvidados([...convidados, usuarioAceito]);
+            setSolicitacoes(solicitacoes.filter((solicitacao) => solicitacao.id_usuario !== id));
         }
     };
 
     const handleDeny = (id: number) => {
         setSolicitacoes(solicitacoes.filter((solicitacao) => solicitacao.id_usuario !== id)); // Remove das solicitações
     };
+
     return (
         <div className="flex min-h-screen bg-gradient-to-b from-[#1C4CDC] to-[#0F2976] ">
             <SidebarMenu />
@@ -130,60 +149,73 @@ export default function DetailsPage() {
                 </div>
 
                 {/* div branca */}
-                <div className="flex flex-col bg-white rounded-lg shadow-lg w-4/5 h-180 mt-25 mb-30">
+                <div className="flex flex-col bg-white rounded-lg shadow-lg w-4/5 h-190 mt-25 mb-30">
 
                     {/* capa */}
                     <img src="/images-travel/capa.png " alt="Description of the image " className='w-full h-80' />
 
                     <div className='p-10 w-full h-full'>
-                        <div className="flex flex-row w-full h-full justify-between">
+                        <div className="flex flex-row w-full justify-between">
 
                             {/* Div com o Organizador */}
 
-                            <div className="flex relative flex-col items-center h-full w-1/4">
+                            <div className="flex relative flex-col items-center w-1/4">
                                 <h2 className="flex items-center justify-center bg-[#D9D9D9] text-[#0F2976] rounded-full py-2 p-5 text-2xl font-bold text-2xl font-bold mb-10">Organizadores</h2>
 
-                                <div className="grid grid-cols-2 gap-4 overflow-x-auto h-60">
-                                    {imagens.map((imagem, index) => (
-                                        <div key={index} className="flex flex-col items-center">
-                                            <img
-                                                src={imagem}
-                                                alt={`Imagem ${index + 1}`}
-                                                className="w-25 h-25 object-cover rounded-full"
-                                            />
-                                            <button
-                                                className="mt-2 px-4 py-2 bg-red-500 text-white rounded-md"
-                                                onClick={() =>
-                                                    setImagens(imagens.filter((_, i) => i !== index))
-                                                }
-                                            >
-                                                Remover
-                                            </button>
-                                        </div>
-                                    ))}
+                                <div className="grid grid-cols-2 gap-2 overflow-x-auto h-60">
+                                    {convidados
+                                        .filter(usuario => usuario.tipo === "Organizador" || usuario.tipo === "OrganizadorPromovido")
+                                        .map((usuario, index) => (
+                                            <div key={usuario.id_usuario} className="flex flex-col items-center">
+                                                <img
+                                                    src={usuario.foto}
+                                                    alt={usuario.nome}
+                                                    className="w-23 h-23 object-cover rounded-full"
+                                                />
+                                                <p className="mt-2 text-sm text-[#292D32]">{usuario.nome}</p>
+                                            </div>
+                                        ))}
 
                                     {/* Botão para adicionar mais Ornizadores Promovidos */}
                                     {imagens.length < 6 && (
-                                        <button
-                                            onClick={() => setIsModalOpen(true)}
-                                            className="w-20 shadow-md h-20 flex items-center justify-center bg-gray-200 rounded-full text-gray-500 hover:bg-gray-300"
-                                        >
-                                            <Plus className='w-13 h-13 text-[#0F2976]' />
-                                        </button>
+                                        <div className='flex flex-col w-full items-center'>
+                                            <button
+                                                onClick={() => setIsModalOpen(true)}
+                                                className="w-19 shadow-md h-19 flex items-center justify-center bg-gray-200 rounded-full text-gray-500 hover:bg-gray-300"
+                                            >
+                                                <Plus className='w-12 h-12 text-[#0F2976]' />
+
+                                            </button>
+                                            <p className="mt-2 text-xs text-[#292D32] whitespace-nowrap">Adicionar Organizador</p>
+                                        </div>
                                     )}
 
                                     <PromoteOrganizerModal
                                         isOpen={isModalOpen}
                                         onClose={() => setIsModalOpen(false)}
                                         usuarios={detalhesViagem.usuario}
-                                        onPromote={(id) => {
+                                        onPromote={async (id) => {
+                                            // Atualiza o tipo do usuário para "OrganizadorPromovido"
+                                            await promoverUsuario(id);
+                                            setConvidados(convidados.map(usuario =>
+                                                usuario.id_usuario === id
+                                                    ? { ...usuario, tipo: "OrganizadorPromovido" }
+                                                    : usuario
+                                            ));
                                             if (!imagens.includes(`/images-home_page/carousel/${id}.png`)) {
                                                 setImagens([...imagens, `/images-home_page/carousel/${id}.png`]);
                                             }
-                                            setIsModalOpen(false); // Fecha o modal
+                                            setIsModalOpen(false);
                                         }}
-                                        onRemove={(id) => {
+                                        onRemove={async (id) => {
+                                            await removerUsuario(id);
+                                            setConvidados(convidados.map(usuario =>
+                                                usuario.id_usuario === id
+                                                    ? { ...usuario, tipo: "Participante" }
+                                                    : usuario
+                                            ));
                                             setImagens(imagens.filter((img) => img !== `/images-home_page/carousel/${id}.png`));
+
                                         }}
                                     />
                                 </div>
@@ -192,17 +224,16 @@ export default function DetailsPage() {
 
                             {/* Div com o gradiente */}
                             <div className="flex flex-col relative items-center justify-center w-full h-100 sm:w-2/3 sm:h-3/3">
-                                <div className="absolute top-0 w-3/5 flex items-center justify-center bg-[#D9D9D9] rounded-full h-12 mb-4">
-                                    {/* Fundo animado */}
+                                <div className="relative w-3/5 flex items-center justify-center bg-[#D9D9D9] rounded-full h-12 mb-10">
+                                    {/* Fundo azul animado */}
                                     <div
-                                        className={`absolute top-0 h-full w-2/4 bg-[#1C4CDC] rounded-full transition-all duration-300 ${activeButton === "convidados" ? "left-0" : "left-1/2"
-                                            }`}
+                                        className={`absolute top-0 left-0 h-12 w-1/2 bg-[#1C4CDC] transition-all duration-300
+                                        ${activeButton === "convidados" ? "rounded-l-full" : "left-1/2 rounded-r-full"}`}
                                     ></div>
-
                                     {/* Botões */}
-                                    <div className="relative flex w-2/3">
+                                    <div className="relative flex w-full">
                                         <button
-                                            className={`z-10 flex items-center justify-center w-1/2 py-2 pr-15 text-2xl font-bold transition-all duration-300 ${activeButton === "convidados"
+                                            className={`z-10 w-1/2 py-2 text-2xl font-bold transition-all duration-300 ${activeButton === "convidados"
                                                 ? "text-white"
                                                 : "text-[#0F2976]"
                                                 }`}
@@ -211,7 +242,7 @@ export default function DetailsPage() {
                                             Convidados
                                         </button>
                                         <button
-                                            className={`z-10 w-1/2 py-2 pl-10 text-2xl font-bold transition-all duration-300 ${activeButton === "solicitacoes"
+                                            className={`z-10 w-1/2 py-2 text-2xl font-bold transition-all duration-300 ${activeButton === "solicitacoes"
                                                 ? "text-white"
                                                 : "text-[#0F2976]"
                                                 }`}
@@ -314,12 +345,14 @@ export default function DetailsPage() {
                                     </div>
                                 </div>
                             </div>
-
-                            <div className='w-1/7 text-[#0F2976] absolute bottom-15'>
-                                <button className='w-full text-2xl font-bold text-center text-[#0F2976] bg-[#D9D9D9] rounded-full px-4 py-2 mt-4'>
-                                    Editar
-                                </button>
-                            </div>
+                        </div>
+                        <div className='w-full flex justify-between mt-8'>
+                            <button className='text-2xl font-bold bg-[#D9D9D9] text-[#0F2976] rounded-full w-60 py-3 hover:bg-gray-300'>
+                                Editar
+                            </button>
+                            <button className='text-2xl font-bold bg-blue-900 text-white rounded-full w-60 py-3 hover:bg-blue-800'>
+                                Encerrar Viagem
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ChevronLeft, X } from "lucide-react";
+import React, { useState } from "react";
+import { ChevronLeft, Search, X } from "lucide-react";
 
 interface User {
     id_usuario: number;
@@ -18,45 +18,90 @@ interface PromoteOrganizerModalProps {
 }
 
 function PromoteOrganizerModal({ isOpen, onClose, usuarios, onPromote, onRemove }: PromoteOrganizerModalProps) {
+    const [search, setSearch] = useState("");
+
+    const usuariosFiltrados = usuarios.filter(usuario =>
+        usuario.nome.toLowerCase().includes(search.toLowerCase())
+    );
+
+    const tipoUsuarioLabel: Record<string, string> = {
+        Organizador: "Organizador",
+        OrganizadorPromovido: "Organizador Promovido",
+        Participante: "Participante",
+    };
+
     if (!isOpen) return null;
+
     return (
         <div
             style={{ backgroundColor: "rgba(41, 45, 50, 0.6)" }}
             className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
         >
 
-            <div className="flex-col bg-white rounded-lg shadow-lg w-[90%] max-w-6xl h-[80vh] p-10 relative justify-center items-center">
+            <div className="relative bg-white rounded-lg shadow-lg w-[90%] max-w-6xl h-[80vh] p-10 relative justify-center items-center">
                 {/* Close Button */}
 
-                <button
-                    onClick={onClose}
-                    className="relative absolute text-gray-500 hover:text-gray-700"
-                >
-                    <ChevronLeft className="w-15 h-15" />
-                </button>
+                <div className="flex flex-row items-center w-full h-20 mb-6">
+                    <button
+                        onClick={onClose}
+                        className="relative absolute text-gray-500 hover:text-gray-700"
+                    >
+                        <ChevronLeft className="w-15 h-15" />
+                    </button>
 
-                <div className="w-full flex flex-col items-center justify-center h-20 relative">
-                    <div className="absolute mt-3  w-2/5 h-15 p-4 bg-[#1C4CDC]"></div>
-                    <div className="absolute h-15 ml-12 w-2/5 h-15 p-4 bg-[#0F2976]">
-                        <h1 className="text-4xl font-bold text-center text-[#00FF4D]">
-                            Adicionar Organizador
-                        </h1>
+                    <div className="relative w-full flex flex-col items-center justify-center h-20">
+                        <div className="absolute mt-4 ml-5  w-2/5 h-15 p-4 bg-[#1C4CDC]"></div>
+                        <div className="absolute h-15 w-2/5 h-15 p-4 bg-[#0F2976]">
+                            <h1 className="text-3xl font-bold text-center text-[#00FF4D]">
+                                Adicionar Organizador
+                            </h1>
+                        </div>
                     </div>
                 </div>
 
                 <div
-                    className="bg-[#D1E1FE] p-6 rounded-lg shadow-md w-4/5 h-3/5 mx-auto "
+                    className="bg-[#D1E1FE] p-6 rounded-lg shadow-md w-4/5 h-4/6 mx-auto mt-20"
                 >
-                    {/* User List */}
-                    <div className="flex flex-col mt-6 p-4">
-                        {usuarios.map((usuario, idx) => {
-                            const isFirst = idx === 0;
-                            const isLast = idx === usuarios.length - 1;
-                            let borderRadius = "";
-                            if (isFirst) borderRadius = "rounded-t-lg";
-                            if (isLast) borderRadius += " rounded-b-lg";
+                    <div>
+                        <div className="relative w-full max-w-md pl-4">
+                            <input
+                                type="text"
+                                placeholder="Buscar usuário"
+                                value={search}
+                                onChange={e => setSearch(e.target.value)}
+                                className="w-80 p-2 pl-12 rounded-md bg-[#0F2976] focus:outline-none text-white placeholder-gray-200"
+                            />
+                            <Search
+                                size={20}
+                                color="#fff"
+                                className="absolute left-8 top-1/2 transform -translate-y-1/2"
+                            />
+                        </div>
+                    </div>
 
-                            // Supondo que usuario.tipo pode ser: "Normal", "OrganizadorPromovido", "Organizador"
+                    <div className="flex flex-row justify-between p-4 ml-3 w-3/5">
+                        <h1 className="text-lg font-bold text-center text-[#0F2976] mt-4">
+                            Nome
+                        </h1>
+                        <h1 className="text-lg font-bold text-center text-[#0F2976] mt-4 ml-2">
+                            Tipo de Usuário
+                        </h1>
+
+                    </div>
+
+                    {/* User List */}
+                    <div className="flex flex-col p-4 overflow-y-auto max-h-72">
+                        {usuariosFiltrados.map((usuario, idx) => {
+                            let borderRadius = "";
+                            if (usuariosFiltrados.length === 1) {
+                                borderRadius = "rounded-lg";
+                            } else {
+                                const isFirst = idx === 0;
+                                const isLast = idx === usuariosFiltrados.length - 1;
+                                if (isFirst) borderRadius = "rounded-t-lg";
+                                if (isLast) borderRadius += " rounded-b-lg";
+                            }
+
                             return (
                                 <div
                                     key={usuario.id_usuario}
@@ -69,14 +114,14 @@ function PromoteOrganizerModal({ isOpen, onClose, usuarios, onPromote, onRemove 
                                             className="w-12 h-12 rounded-full mr-4"
                                         />
                                         <div className="flex flex-col min-w-0">
-                                            <p className="font-bold truncate">{usuario.nome}</p>
-                                            <p className="text-sm text-gray-500 truncate">{usuario.email}</p>
+                                            <p className="font-bold text-lg truncate">{usuario.nome}</p>
+                                            {/* <p className="text-sm text-gray-500 truncate">{usuario.email}</p> */}
                                         </div>
-                                        
+
                                     </div>
 
-                                    <div className="flex items-center w-1/3 justify-start">
-                                        <p className="text-md text-left">{usuario.tipo}</p>
+                                    <div className="flex items-center w-2/5 justify-start">
+                                        <p className="text-md text-left">{tipoUsuarioLabel[usuario.tipo]}</p>                                    
                                     </div>
 
                                     <div className="flex gap-2 pr-2 min-w-[120px] justify-end">
