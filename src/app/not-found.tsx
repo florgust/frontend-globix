351e5318d37b38fef62c1590339d739300af5ece
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-06-06 16:54:27 -0300

    SCRUM-155: Página de NotFound funcionando

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,13 @@
 "use client";
 import React from "react";
-import SidebarMenu from "../../components/ui/SidebarMenu";
+import SidebarMenu from "../components/ui/SidebarMenu";
 import { HeaderPages } from "@/components/ui/header";
 import Footer from "@/components/ui/Footer";
+import { useRouter } from "next/navigation";
 
-export default function TripTransport() {
+export default function NotFound() {
+    const router = useRouter();
+
     return (
         <div className="flex min-h-screen bg-gradient-to-b from-[#1C4CDC] to-[#0F2976]">
             <SidebarMenu />
@@ -26,12 +29,22 @@ export default function TripTransport() {
                 </div>
 
                 <div className="flex justify-center space-x-4 gap-5 text-[#FFFFFF] mt-87 mb-40">
-                    <button className="w-70 h-14 border-2 border-[#FFFFFF] rounded-[6.25rem] cursor-pointer hover:scale-110 flex items-center justify-center gap-2">                            <img
-                        src="/images-error_404/seta.svg"
-                        alt="Seta"
-                        className="w-5 h-5"
-                    />
-                        <a href="/home_page" className="text-lg">Voltar para Página Inicial</a>
+                    <button
+                        className="w-70 h-14 border-2 border-[#FFFFFF] rounded-[6.25rem] cursor-pointer hover:scale-110 flex items-center justify-center gap-2"
+                        onClick={() => router.push("/home_page")}
+                    >
+                        <img
+                            src="/images-error_404/seta.svg"
+                            alt="Seta"
+                            className="w-5 h-5"
+                        />
+                        <span className="text-lg">Voltar para Página Inicial</span>
+                    </button>
+                    <button
+                        className="w-72 h-14 border-2 border-[#FFFFFF] rounded-[6.25rem] cursor-pointer hover:scale-110 flex items-center justify-center gap-2 bg-transparent"
+                        onClick={() => router.back()}
+                    >
+                        <span className="text-lg">Voltar para página anterior</span>
                     </button>
                 </div>
                 <Footer />
