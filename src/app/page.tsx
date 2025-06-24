aaf3f2d27bd67c0efe12b1a94d3a89b254e636df
florgust <luan.flor05@gmail.com>
2025-06-24 11:04:32 -0300

    SCRUM-187: ajuste erro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-import { redirect } from "next/navigation";
+"use client"
+
+import { useEffect } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  redirect("/initial");
+  const router = useRouter();
+
+  useEffect(() => {
+    const usuario = Cookies.get("usuario");
+    if (usuario) {
+      router.replace("/home_page");
+    } else {
+      router.replace("/initial");
+    }
+  }, [router]);
+
   return null;
 }
