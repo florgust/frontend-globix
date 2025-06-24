7749cb4f4cac233d5d1ab1d4e64ef0fafaf75e66
florgust <luan.flor05@gmail.com>
2025-06-24 13:47:02 -0300

    SCRUM-158: ajuste na criacao de conta

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -82,7 +82,7 @@ export default function RegisterPage() {
         };
 
         try {
-            const response = await api.post('https://globix-afaea8fe15ce.herokuapp.com/usuario', payload, {
+            const response = await api.post('/usuario', payload, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
