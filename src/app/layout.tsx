efce41243d24dc7affce0da94b5181b3e87329b4
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2025-07-05 04:28:54 -0300

    SCRUM-203: Adicionando FavIcon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,20 @@
-// src/app/layout.tsx
-
-import '../styles/global.css'; // Importa o arquivo de estilo global, onde o Tailwind está configurado
+import '../styles/global.css';
 import { ReactNode } from 'react';
+
+export const metadata = {
+  title: "Globix",
+  description: "Seu app de viagens",
+  icons: {
+    icon: "/favicon.png", 
+  },
+};
 
 export default function Layout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <html lang="pt-BR" className="h-full">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className="h-full text-gray-900 overflow-x-hidden">
         <main className="h-full">{children}</main>
       </body>
