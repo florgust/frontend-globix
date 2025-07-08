b6e332ac4162de6567a351f95f899596232c31cd
arthuramos <rmsarthur1@gmail.com>
2025-07-08 17:30:08 -0300

    Corrigindo a exibição da data no TripCard

--- src/components/ui/TripCard.tsx
+++ src/components/ui/TripCard.tsx
@@ -50,13 +50,13 @@ const TripCard = ({ trip }: TripCardProps) => {
         </div>
         <p>
           <strong>Dia:</strong>{" "}
-          {new Date(trip.dataInicio).toLocaleDateString("pt-BR")}
+            {new Date(new Date(trip.dataInicio).setDate(new Date(trip.dataInicio).getDate() + 1)).toLocaleDateString("pt-BR")}
         </p>
         <p><strong>Organizador:</strong> {trip.organizador}</p>
         <p><strong>Transporte:</strong> {trip.transporte}</p>
         <p>
           <strong>Duração:</strong>{" "}
-          {new Date(trip.dataInicio).toLocaleDateString("pt-BR")} até {new Date(trip.dataFim).toLocaleDateString("pt-BR")}
+            {new Date(new Date(trip.dataInicio).setDate(new Date(trip.dataInicio).getDate() + 1)).toLocaleDateString("pt-BR")} até {new Date(new Date(trip.dataFim).setDate(new Date(trip.dataFim).getDate() + 1)).toLocaleDateString("pt-BR")}
         </p>
         <button
           className={`px-3 py-1 rounded-xl mt-2 text-sm cursor-pointer ${
